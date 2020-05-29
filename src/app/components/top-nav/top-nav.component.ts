b4b8b644c93175b24dd40c99a6568d5a146bda86
LordTofi <tofaniulian1@gmail.com>
2020-05-29 20:55:12 +0300

    Implemented theming

--- src/app/components/top-nav/top-nav.component.ts
+++ src/app/components/top-nav/top-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-top-nav',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavComponent implements OnInit {
 
+  openSettings: boolean = false;
+  theme: string;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.theme = localStorage.getItem("theme") 
+
+    if(!this.theme) {
+      this.theme = "light";
+      localStorage.setItem("theme", "light")
+    }
+
+    var body = document.body;
+    body.classList.add(this.theme);
   }
 
+  togleTheme() {
+    var body = document.body;
+    body.classList.remove(this.theme)
+    this.theme = this.theme == "light" ? "dark" : "light";
+    localStorage.setItem("theme", this.theme)
+    body.classList.add(this.theme);
+  }
 }
