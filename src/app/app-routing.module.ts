77ca811fa4002a7d774df6f29c7b356930b46baa
LordTofi <tofaniulian1@gmail.com>
2020-05-29 17:08:50 +0300

    Added Top Nav + basic feed/calendar/map components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FeedComponent } from './components/feed/feed.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { MapComponent } from './components/map/map.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'feed', component: FeedComponent},
+  { path: 'calendar', component: CalendarComponent},
+  { path: 'map', component: MapComponent},
+  { path: '', component: FeedComponent, pathMatch: 'full' }, //to do: redirect to login
+  { path: '**', component: FeedComponent } //to do: actually might do a PageNotFoundComponent
+];       
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    FeedComponent,
+    CalendarComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
