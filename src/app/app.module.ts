26e851b42db9ecae011bbeeeca65e6716b48f28e
LordTofi <tofaniulian1@gmail.com>
2020-05-30 10:07:10 +0300

    Creating a calendar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
 import { FeedComponent } from './components/feed/feed.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
+import { CalendarComponent, ChunkPipe } from './components/calendar/calendar.component';
 import { MapComponent } from './components/map/map.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,11 +15,13 @@ import { MapComponent } from './components/map/map.component';
     TopNavComponent,
     FeedComponent,
     CalendarComponent,
-    MapComponent
+    MapComponent,
+    ChunkPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
